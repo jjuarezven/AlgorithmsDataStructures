dfe2c5a25035687a3ccd38552e18dfc0875f003c
josejuarez <79205538+josejmill5@users.noreply.github.com>
2022-11-30 16:05:10 -0500

    Implementing stack

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Stack;
+
+var stack = new StackImplementation();
+stack.Push(4);
+stack.Push(3);
+stack.Push(2);
+stack.Push(1);
+stack.Traverse();
+Console.WriteLine();
+Console.WriteLine($"Pop: {stack.Pop()}");
+Console.WriteLine($"Peek: {stack.Peek()}");
+Console.WriteLine($"Count: {stack.Count}");
+stack.Traverse();
+Console.WriteLine($"Pop: {stack.Pop()}");
+Console.WriteLine($"Pop: {stack.Pop()}");
+Console.WriteLine($"Pop: {stack.Pop()}");
+Console.WriteLine($"Pop: {stack.Pop()}");
--- Stack/Stack.cs
+++ Stack/Stack.cs
@@ -1,12 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Common;
 
 namespace Stack
 {
-    internal class Stack
+    internal class StackImplementation
     {
+        private Node? head;
+        private Node? current;
+        public int Count { get; private set; }
+
+        public StackImplementation()
+        {
+            head = new Node();
+        }
+
+        // Push is adding a node in the beginning of the collection
+        public void Push(int value)
+        {
+            var newNode = new Node
+            {
+                Data = value,
+                // preserve the reference to the current "next" of the head
+                Next = head.Next,
+            };
+            head.Next = newNode;
+            Count++;
+        }
+
+        public int Pop()
+        {
+            int result;
+            if (head.Next is not null)
+            {
+                current = head.Next;
+                result = current.Data;
+                
+                head.Next = current.Next;
+                current.Next = null;
+                Count--;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            return result;
+        }
+
+        public int Peek()
+        {
+            int result;
+            if (head.Next is not null)
+            {
+                current = head.Next;
+                result = current.Data;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            return result;
+        }
+
+        public void Traverse()
+        {
+            current = head;
+            while (current.Next is not null)
+            {
+                current = current.Next;
+                Console.WriteLine($"{current.Data}");                
+            }
+        }
     }
 }
